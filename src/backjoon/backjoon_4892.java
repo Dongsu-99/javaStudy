5afc821a5e371e35c0a261e1fc777f3b15ee65d7
Dongsu-99 <a3170682@gmail.com>
2023-03-23 22:26:01 +0900

    Day - 2 commit

---
+++ src/backjoon/backjoon_4892.java
@@ -0,0 +1,31 @@
+package backjoon;
+import java.util.Scanner;
+public class backjoon_4892 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count =1;
+        for (; ; ) {
+
+            int n = sc.nextInt();
+            if (n == 0) {
+                break;
+            }
+            // 홀수 계산식 : n0 * 3 + 1 / 2 * 3 / 9
+            // 홀수
+            if (n % 2 != 0) {
+                n = (((n * 3) + 1) / 2) * 3 / 9;
+                System.out.println(count + ". odd " + n);
+                //짝수
+            } else {
+                n = (((n * 3) / 2) * 3) / 9;
+                System.out.println(count + ". even " + n);
+
+
+            }
+            count++;
+        }
+
+    }
+}
+
+
---
+++ src/backjoon/backjoon_5338.java
@@ -0,0 +1,11 @@
+package backjoon;
+
+public class backjoon_5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
