bc61c0372aa6f154b50777dea5ee26c1a3035d9c
Dongsu-99 <a3170682@gmail.com>
2023-03-31 18:19:05 +0900

    Day - 10 commit

--- src/baekjoon/baekjoon_4562.java
+++ src/baekjoon/baekjoon_4562.java
@@ -1,15 +1,17 @@
 package baekjoon;
 import java.util.Scanner;
 public class baekjoon_4562 {
-//    첫번째 입력받은 수가 두번째 입력받은 수보다 작으면 "NO BRAINS", 아니면 "MMM BRAINS"을 출력하면 되는 간단한 문제입니다.
+//    첫번째 입력받은 수가 두번째 입력받은 수보다 작으면 "NO BRAINS", 아니면 "MMM BRAINS"을 출력
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        while(true){
+        for(int i = 0; i<n; i++){
             int x =sc.nextInt();
             int y =sc.nextInt();
-            System.out.println(x < y ? "NO":"MMM");
+            System.out.println(x < y ? "NO BRAINS":"MMM BRAINS");
         }
+
+
     }
 
 
