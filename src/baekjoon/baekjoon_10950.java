64a96a0325ad0b31fe77621271f6a899aaa7665a
Dongsu-99 <a3170682@gmail.com>
2023-04-13 18:08:15 +0900

    Day - 22 commit

---
+++ src/baekjoon/baekjoon_10950.java
@@ -0,0 +1,16 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class baekjoon_10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int TC = sc.nextInt();
+
+        for(int i = 1; i<=TC; i++){
+            int num1 =sc.nextInt();
+            int num2 =sc.nextInt();
+            System.out.println(num1+num2);
+        }
+    }
+}
